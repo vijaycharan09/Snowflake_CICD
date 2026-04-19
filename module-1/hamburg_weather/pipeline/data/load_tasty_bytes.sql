d0fc469fbeed2b8931edc97410473ae7d97d8127
vijaycharan09 <vijaycharan@gmail.com>
2026-04-18 23:08:29 -0400

    New Changes

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -64,7 +64,7 @@ CREATE OR REPLACE TABLE {{env}}_tasty_bytes.raw_pos.country
    iso_currency VARCHAR(3),
    iso_country VARCHAR(2),
    city VARCHAR(2000),
-   city_population VARCHAR(2000),
+   city_population VARCHAR(1000),
    CITY_ID NUMBER(18,0)
 );
 
@@ -294,17 +294,17 @@ USE WAREHOUSE demo_build_wh;
 
 
 -- country table load
--- COPY INTO {{env}}_tasty_bytes.raw_pos.country
--- (
---    country_id,
---    country,
---    iso_currency,
---    iso_country,
---    city_id,
---    city,
---    city_population
--- )
--- FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
+ COPY INTO {{env}}_tasty_bytes.raw_pos.country
+ (
+    country_id,
+    country,
+    iso_currency,
+    iso_country,
+    city_id,
+    city,
+    city_population
+ )
+ FROM @{{env}}_tasty_bytes.public.s3load/raw_pos/country/;
 
 
 -- franchise table load
